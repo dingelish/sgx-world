51e5956d9a527da41fe8ef30017a233d9158f07e
Yu Ding <dingelish@gmail.com>
2019-12-17 21:12:45 -0800

    Update to v1.1.0

--- check-all.sh
+++ check-all.sh
@@ -6,11 +6,16 @@ submodules=`find . -maxdepth 1 -not -path '*/\.*' -not -path '.' -type d | xargs
 
 for DD in ${submodules[@]}; do
     cd ${DD} && \
-    echo ${DD} && git fetch && git status | grep "up to date"
-    #echo ${DD} && git checkout master
-    retval=$?
-    if [ $retval -ne 0 ]; then
-        echo "Error " ${DD}
-    fi
+    brch=`git branch | grep '\*' | cut -d ' ' -f 2`
+    #if [ $brch = "master" ]; then
+        echo ${DD} && git fetch && git status | grep "up to date"
+        #echo ${DD} && git checkout master
+        retval=$?
+        if [ $retval -ne 0 ]; then
+            echo "Error " ${DD}
+        fi
+    #else
+    #    echo working on ${DD} and brch=$brch skipping...
+    #fi
     cd ..
 done
